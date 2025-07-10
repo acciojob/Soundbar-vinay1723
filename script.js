9c0c454866c083bf8a050af1cb651c7c97b575e1
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-07-10 18:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //your JS code here. If required.
-const sounds = ['applause', 'boo', 'gasp', 'tada','victory','wrong'];
+// const sounds = ['applause', 'boo', 'gasp', 'tada','victory','wrong'];
 
-const audioElements = {};
+// const audioElements = {};
 
-sounds.forEach(sound => {
-  audioElements[sound] = new Audio(`./sounds/${sound}.mp3`);
-});
+// sounds.forEach(sound => {
+//   audioElements[sound] = new Audio(`./sounds/${sound}.mp3`);
+// });
 
-document.querySelectorAll('.btn').forEach(button => {
-  button.addEventListener('click', () => {
-    const sound = button.getAttribute('data-sound');
-    stopAllSounds();
-    // audioElements[sound].currentTime = 0; // rewind
-    audioElements[sound].play();
-  });
-});
+// document.querySelectorAll('.btn').forEach(button => {
+//   button.addEventListener('click', () => {
+//     const sound = button.getAttribute('data-sound');
+//     stopAllSounds();
+//     // audioElements[sound].currentTime = 0; // rewind
+//     audioElements[sound].play();
+//   });
+// });
 
-document.querySelector('.stop').addEventListener('click', stopAllSounds);
+// document.querySelector('.stop').addEventListener('click', stopAllSounds);
 
-function stopAllSounds() {
-  Object.values(audioElements).forEach(audio => {
-    audio.pause();
-    // audio.currentTime = 0;
-  });
-}
+// function stopAllSounds() {
+//   Object.values(audioElements).forEach(audio => {
+//     audio.pause();
+//     // audio.currentTime = 0;
+//   });
+// }
