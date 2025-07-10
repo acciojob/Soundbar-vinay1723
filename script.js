7fa3f6098879b79384be66036a1d10c323613bbf
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-07-10 17:22:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const sounds = ['clap', 'kick', 'snare', 'boom'];
+
+const audioElements = {};
+
+sounds.forEach(sound => {
+  audioElements[sound] = new Audio(`./sounds/${sound}.mp3`);
+});
+
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const sound = button.getAttribute('data-sound');
+    stopAllSounds();
+    audioElements[sound].currentTime = 0; // rewind
+    audioElements[sound].play();
+  });
+});
+
+document.querySelector('.stop').addEventListener('click', stopAllSounds);
+
+function stopAllSounds() {
+  Object.values(audioElements).forEach(audio => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
